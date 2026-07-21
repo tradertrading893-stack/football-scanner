9e93322901f8df893fd0f558452425856938876a
tradertrading893-stack <tradertrading893@gmail.com>
2026-07-22 02:47:53 +0300

    Update filters.py

--- filters.py
+++ filters.py
@@ -1,18 +1,15 @@
-def check_team_a(stats):
-    return (
-        stats["shots"] >= 34 and
-        stats["shots_on_target"] >= 15 and
-        stats["corners"] >= 10
-    )
-
-
-def check_team_b(stats):
-    return (
-        stats["shots"] >= 31 and
-        stats["shots_on_target"] >= 14 and
-        stats["corners"] >= 10
-    )
-
-
 def match_passes_filter(team_a, team_b):
-    return check_team_a(team_a) and check_team_b(team_b)
+
+    team_a_ok = (
+        team_a["shots"] >= 34 and
+        team_a["shots_on_target"] >= 15 and
+        team_a["corners"] >= 10
+    )
+
+    team_b_ok = (
+        team_b["shots"] >= 31 and
+        team_b["shots_on_target"] >= 14 and
+        team_b["corners"] >= 10
+    )
+
+    return team_a_ok and team_b_ok
