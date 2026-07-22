37b7d91210eb122096c3011fd0ee2b66d50bc824
tradertrading893-stack <tradertrading893@gmail.com>
2026-07-22 03:08:53 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,24 @@
-from telegram import Bot
-from config import BOT_TOKEN, CHAT_ID
+import os
+import requests
+
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 
 async def send_message(text):
-    bot = Bot(token=BOT_TOKEN)
-    await bot.send_message(
-        chat_id=CHAT_ID,
-        text=text
-    )
+
+    if not BOT_TOKEN or not CHAT_ID:
+        print("BOT_TOKEN ან CHAT_ID არ მოიძებნა")
+        return
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+
+    response = requests.post(url, data=data)
+
+    print(response.text)
